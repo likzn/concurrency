0d1d8e79c7f0cfb5f7300cc0b36403c1bb3a66ad
likzn <532573425@qq.com>
2019-02-14 21:37:04 +0800

    annotation

--- src/main/java/com/likzn/concurrency/annotation/NotThreadSafe.java
+++ src/main/java/com/likzn/concurrency/annotation/NotThreadSafe.java
@@ -5,7 +5,9 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
-
+/*
+    用来标记{不安全线程}的注解，对于代码没有用处，方便查看
+ */
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.SOURCE)
 public @interface NotThreadSafe {
